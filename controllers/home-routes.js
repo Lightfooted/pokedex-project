b3c195a83b9c76e648fab3d2f23734e163548acc
Lightfooted <vesselofbalance@hotmail.com>
2021-09-26 02:42:16 -0400

    figured out how to route a pokemon to homepage. need to finish the rest of route.get

---
+++ controllers/home-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+//router.get('/', (req, res) => {
+  //res.render('homepage');
+//});
+
+//test
+
+router.get('/', (req, res) => {
+  res.render('homepage', {
+    id: 1,
+    name: 'Bulbasaur',
+    height: 7,
+    weight: 69,
+    front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+    entry_number: 1,
+    user: {
+      username: 'Ash'
+    }
+  });
+});
+
+module.exports = router;
