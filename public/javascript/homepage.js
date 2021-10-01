ccad2943e822ec3333d4b84d803375981f094939
Lightfooted <vesselofbalance@hotmail.com>
2021-10-01 00:03:21 -0400

    need to fix music

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,4 +1,4 @@
  //this function plays pokemon center music from original pokemon game 
- window.onload=function(){
-    document.getElementById("my_audio").play();
-  }
+//  window.onload=function(){
+//     document.getElementById("my_audio").play();
+//   }
